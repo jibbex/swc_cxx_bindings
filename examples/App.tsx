25428ed99e0c10e2571174bac8079020b30c7fa3
Manfred Michaelis <mm@michm.de>
2025-04-01 23:08:10 +0200

    fix: added missing type on click handler

--- examples/App.tsx
+++ examples/App.tsx
@@ -3,12 +3,12 @@ import * as React from 'react';
 export const App = () => {
 	const [click, setClick] = React.useState(0);
 	
-	const handleClick: () => void = () => setClick(num => num++);
+	const handleClick: () => void = () => setClick((num: number) => num++);
 
 	return (
-		<>
+		<div>
 			<h1>{click}</h1>
-			<button onClick={handleClick}>increase</button
-		</>
+			<button onClick={handleClick}>increase</button>
+		</div>
 	);
 };  
